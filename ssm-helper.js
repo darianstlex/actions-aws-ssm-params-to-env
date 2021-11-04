ff3b429750210901e786e7981d2f0ef5d0a21bea
darianstlex <ivan@motorway.co.uk>
2021-11-04 18:05:52 +0000

    ssm client

--- ssm-helper.js
+++ ssm-helper.js
@@ -1,28 +1,17 @@
-const AWS = require('aws-sdk');
+import * as AWS from '@aws-sdk/client-ssm';
 
 const getParameters = async (ssmPath, getChildren, decryption, region) => {
-  AWS.config.update({region: region});
-  const ssm = new AWS.SSM();
-  let params;
-  let promise;
-  if (getChildren) {
-    params =
-      {
-        Path: ssmPath,
-        WithDecryption: decryption,
-      };
-    promise = ssm.getParametersByPath(params).promise();
-  } else {
-    params =
-      {
-        Names: [ssmPath],
-        WithDecryption: decryption,
-      };
-    promise = ssm.getParameters(params).promise();
-    ;
-  }
-  const result = await promise;
-  return result.Parameters;
+  const ssm = new AWS.SSM({ region });
+
+  const promise = getChildren
+    ? await ssm.getParametersByPath({
+    Path: ssmPath,
+    WithDecryption: decryption,
+  }) : await ssm.getParameters({
+    Names: [ssmPath],
+    WithDecryption: decryption,
+  })
+  return promise.Parameters;
 };
 
 module.exports = {getParameters};
